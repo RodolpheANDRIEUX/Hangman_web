a88e98aa1168ae76396b6b6dbf1d8a7a372a3082
Florian ALBORA <31florian974@gmail.com>
2022-11-18 15:44:04 +0100

    add(#2): added hangman for web
    modify main.go
    add run.go
    add words.txt
    add gameFunctions

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+const port = ":8080"
+
+func main() {
+
+	/*	rand.Seed(time.Now().UnixNano())
+
+		fmt.Println("(http://localhost:8080) - Server started on port", port)
+
+		http.HandleFunc("/", home)
+
+		http.HandleFunc("/replay", replay)
+
+		http.ListenAndServe(port, nil)*/
+
+}
