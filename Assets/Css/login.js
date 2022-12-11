6dc39e8b10ea3300c42759d10db67222e77c3555
Florian ALBORA <31florian974@gmail.com>
2022-12-11 21:21:08 +0100

    add(18): Login - Signup
    add css
    add js
    add login button animation

---
+++ Assets/Css/login.js
@@ -0,0 +1,25 @@
+const login_form = document.getElementById('login-form');
+const signup_form = document.getElementById('sinup-form');
+const container_login_form = document.getElementById('container-login-form')
+const container_play_form = document.getElementById('container-play-form')
+
+const showLoginForm = () => {
+    login_form.style.display = "grid"
+    container_login_form.style.width = "20em"
+    signup_form.style.display = "none"
+    container_play_form.style.width = "10em"
+}
+
+const showSignupForm = () => {
+    signup_form.style.display = "grid"
+    container_play_form.style.width = "20em"
+    login_form.style.display = "none"
+    container_login_form.style.width = "10em"
+}
+
+const closeAll = () => {
+    login_form.style.display = "none"
+    signup_form.style.display = "none"
+    container_login_form.style.width = "10em"
+    container_play_form.style.width = "10em"
+}
