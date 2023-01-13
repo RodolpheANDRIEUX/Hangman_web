30e31c30e68535e0306d11f8c59ab4c429afd878
Florian ALBORA <31florian974@gmail.com>
2023-01-13 16:49:31 +0100

    add(25): add layout
    add bg overlay to close all tabs ( login page )

--- Assets/Script/login.js
+++ Assets/Script/login.js
@@ -1,14 +1,20 @@
 const signup_form = document.getElementById('sinup-form');
 const container_play_form = document.getElementById('container-play-form')
+const bg_overlay = document.getElementById('bg-overlay')
 
 const showSignupForm = () => {
     signup_form.style.display = "grid"
     container_play_form.style.width = "20em"
+    bg_overlay.style.display = "grid"
+    bg_overlay.style.zIndex = "0"
 }
 
 const closeAll = () => {
     signup_form.style.display = "none"
     container_play_form.style.width = "10em"
+    bg_overlay.style.display = "none"
+    bg_overlay.style.zIndex = "-1"
+    popup.classList.remove('opened');
 }
 
 const popup = document.getElementById("login-popup");
